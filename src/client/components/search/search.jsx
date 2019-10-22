7e3850fc227e77b68d94a03788558ac11269c96d
kinskin <md.asshikin@gmail.com>
2019-10-22 23:48:36 +0800

    able to gfetch api

--- src/client/components/search/search.jsx
+++ src/client/components/search/search.jsx
@@ -6,9 +6,26 @@ import Display from './display/display.jsx'
 
 export default class Search extends React.Component{
 
+    constructor(){
+        super()
+        this.state = {
+            searchValue: '',
+            apiKey: '7i4KgSemwP8WZSYtPePlpVoVbJeHE8EO'
+        }
+    }
 
     searchValue(value){
-        console.log('this is the value in the search component: ', value)
+        let apiKey = this.state.apiKey
+        let url = `http://api.giphy.com/v1/gifs/search?q=${value}&api_key=${apiKey}`
+        fetch(url,{
+            header:{
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            }
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(err => console.log('error: ', err))
     }
 
 
